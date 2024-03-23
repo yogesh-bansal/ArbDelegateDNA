95d173da988ef93b5be64a32e343e51353c0aba8
yogesh-bansal <yogesh.bansal@live.com>
2024-03-23 12:51:13 +0530

    ui Note

--- ui.R
+++ ui.R
@@ -12,7 +12,7 @@ ui <- fluidPage(theme = bs_theme(bootswatch = "sandstone"),
   br(),
   fluidRow(
     column(width = 3,tags$a(target = "_blank", href="https://thankarb.com/", tags$img(src = "images/AF_lockup_navy.png", height="80px")),align="left"),
-    column(width = 6,h2("Delegate Network Visualization Based on Voting Patterns")),
+    column(width = 6,h2("Delegate Network Visualization Based on Voting Patterns"),align="center"),
     column(width = 3,tags$a(target = "_blank", href="https://app.dework.xyz/datagrants-thankar", tags$img(src = "images/odcarb.png", height="100px")),align="right"),
   ),
   tabsetPanel(
@@ -20,7 +20,7 @@ ui <- fluidPage(theme = bs_theme(bootswatch = "sandstone"),
       br(),
       sidebarLayout(
         sidebarPanel(width=3,
-          p(" Delegate is recognized as having a minimum of 1 Arb voting power, coupled with a prerequisite of having at least 2 addresses delegating to them."),
+          p("Delegate is recognized as having a minimum of 1 Arb voting power, coupled with a prerequisite of having at least 2 addresses delegating to them."),
           p("From a pool of 153 Snapshot Proposals and 28 tally proposals, we've carefully curated 96 proposals characterized by diverse voting patterns. This selection criterion ensures that no proposal garners more than 80% of votes for a single answer."),
           p("The set of responses to the aforementioned 96 proposals is referred to as the 'Delegate DNA' vector."),
           p("The Similarity Score, an essential metric, is computed as the ratio of shared voting instances between two delegates to the total number of proposals they've jointly engaged with. This calculation is based on the aforementioned 'Delegate DNA' vector."),
@@ -30,6 +30,7 @@ ui <- fluidPage(theme = bs_theme(bootswatch = "sandstone"),
           sliderInput("min_comprop", label = "Minimum Shared Voted Proposals", min = 10, max = 90, value = 80,step=5)
         ),
         mainPanel(width=9,
+          column(width = 12,p("Node Size is based on Number of votes by the Delegate while Node color shows the number of delegators for a Delegate"),align="center"),
           withSpinner(forceNetworkOutput("coll_network",height="700px"))
         )
       )
@@ -39,7 +40,6 @@ ui <- fluidPage(theme = bs_theme(bootswatch = "sandstone"),
       fluidPage(
           downloadButton("downloadData", "Download"),
           withSpinner(dataTableOutput("coll_data"))
-          
       )
     ),
   )
